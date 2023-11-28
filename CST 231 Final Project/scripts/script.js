1863e3f2550a658afc0fe1f329f07e7b77557f79
Paul Feaster <111462314+plfeaster@users.noreply.github.com>
2023-11-27 20:45:20 -0500

    Add files via upload

---
+++ CST 231 Final Project/scripts/script.js
@@ -0,0 +1,10 @@
+/*
+Student Name: Paul Feaster, Darrin Briscoe
+File Name: script.js
+Date: 11/26/2023
+*/
+
+//Hamburger menu function
+function hamburger() {
+    var menu = document.getElementById("menu")
+}

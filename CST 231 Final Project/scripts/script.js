41561bfef29f97647b0853bcee169462dd7a9a78
Paul Feaster <111462314+plfeaster@users.noreply.github.com>
2023-11-28 17:19:09 -0500

    Add files via upload

---
+++ CST 231 Final Project/scripts/script.js
@@ -0,0 +1,12 @@
+/*
+Student Name: Paul Feaster, Darrin Briscoe
+File Name: script.js
+Date: 11/26/2023
+*/
+
+//Global variables
+
+function hamburger() {
+    var menu = document.getElementById("menu-icon")
+    if (menu.style.display === "block") ;
+}
